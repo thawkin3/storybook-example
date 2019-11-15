7776706b681afd6717da67f6fb96a366aa0adf09
Tyler Hawkins <thawkins@instructure.com>
2019-11-15 15:13:08 -0700

    adds css fixes for long strings after doing manual testing in storybook

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -28,3 +28,27 @@ export const underline = () => (
     focusType="underline"
   />
 );
+
+export const withLongTextOutline = () => (
+  <Button
+    text="Click me. But think about it first. Don't just go around clicking things. That's dangerous. Now this button text is getting pretty lengthy."
+    onClick={action('clicked!')}
+    focusType="outline"
+  />
+);
+
+export const withLongTextUnderline = () => (
+  <Button
+    text="Click me. But think about it first. Don't just go around clicking things. That's dangerous. Now this button text is getting pretty lengthy."
+    onClick={action('clicked!')}
+    focusType="underline"
+  />
+);
+
+export const withNoSpacesInText = () => (
+  <Button
+    text="sdhfkjhgfdkjghdfjghdflkfdjskdlafsjlkfghfslkdjadjkfhgljsdajhklfgfhdjfkhdajkdfghjkfahdadjkfhafsjkhddfgjlhfasdlkfah"
+    onClick={action('clicked!')}
+    focusType="outline"
+  />
+);
